1e4204bfb3a1fc234273693adc50cf79ffd4c867
Yuki <yuki08281993@gmail.com>
2018-01-31 18:40:06 +0900

    Adopt Jekyll + Haml + SCSS

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@ SITE_ROOT=`cat secret/site_root`
 SERVER=`cat secret/server`
 ACCOUNT=`cat secret/account`
 PASSWORD=`cat secret/password`
+jekyll build
 sshpass -p${PASSWORD} ssh ${ACCOUNT}@${SERVER} "cd yuchiki.net; ls | grep -v 'kantan-cgi' |xargs rm -r"
-sshpass -p${PASSWORD} scp -r ./root/* ${ACCOUNT}@${SERVER}:${SITE_ROOT}
+sshpass -p${PASSWORD} scp -r ./_site/* ${ACCOUNT}@${SERVER}:${SITE_ROOT}
