d65b3cec7c090d59f16a0aec2e1e5205bde35c1c
Yuki <yuki08281993@gmail.com>
2018-02-03 19:15:39 +0900

    add post-update hook for deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,21 @@
-SITE_ROOT=`cat secret/site_root`
-SERVER=`cat secret/server`
-ACCOUNT=`cat secret/account`
-PASSWORD=`cat secret/password`
-jekyll build
-sshpass -p${PASSWORD} ssh ${ACCOUNT}@${SERVER} "cd yuchiki.net; ls | grep -v 'kantan-cgi' |xargs rm -r"
-sshpass -p${PASSWORD} scp -r ./_site/* ${ACCOUNT}@${SERVER}:${SITE_ROOT}
+#! /usr/bin/zsh -eu
+set -x
+
+branch=$(git symbolic-ref --short HEAD)
+
+case $branch in
+    master)
+        SITE_ROOT=`cat secret/site_root`
+        SERVER=`cat secret/server`
+        ACCOUNT=`cat secret/account`
+        PASSWORD=`cat secret/password`
+
+        echo "Build with Jekyl."
+        jekyll build
+
+        echo "Deploy to the server."
+        sshpass -p${PASSWORD} ssh ${ACCOUNT}@${SERVER} "cd yuchiki.net; ls | grep -v 'kantan-cgi' |xargs rm -r"
+        sshpass -p${PASSWORD} scp -r ./_site/* ${ACCOUNT}@${SERVER}:${SITE_ROOT}
+        ;;
+    *) echo "No deploy-task for branch ${branch}.";;
+esac
